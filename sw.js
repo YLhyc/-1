e17115bfb344b62ceba77674cc2c243338dc781e
YLhyc <23756@qq.com>
2026-07-07 14:47:42 +0800

    修复晨间模式第2轮审查5个问题：游标推进时机/暂停加载保护/轮询失败回调/导出内联morning/replay点击计数

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kv-20260707-143453';
+const CACHE = 'kv-20260707-144659';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
