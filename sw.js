e05c1a27b80383e39be83b1da9597cf1e2a57add
YLhyc <23756@qq.com>
2026-07-08 09:38:34 +0800

    晨读播放器：困难/熟了不中断播放 + 新增上一个按钮 + 统计防重复

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kv-20260707-164305';
+const CACHE = 'kv-20260708-093643';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
