7314b4cda2780233fd4a11f042fb21acf9a87d96
YLhyc <23756@qq.com>
2026-07-13 19:39:46 +0800

    修复 iPhone 图标裁切

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kv-20260713-174400';
+const CACHE = 'kv-20260713-192950';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
