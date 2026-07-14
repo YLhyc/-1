9cd4744b1d3ca630083c059c79409ee39e75a1c6
YLhyc <2375681475@qq.com>
2026-07-14 16:59:25 +0800

    发布首页继续学习与红宝书交互修复

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kv-20260714-145009';
+const CACHE = 'kv-20260714-151126';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
